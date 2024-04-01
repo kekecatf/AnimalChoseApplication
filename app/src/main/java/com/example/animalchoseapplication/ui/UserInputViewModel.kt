15482b57c3f1da27f7a8871198cb52be24825df4
Atif Kekeç <kekecatf@outlook.com>
2024-04-01 14:22:21 +0300

    day2

--- app/src/main/java/com/example/animalchoseapplication/ui/UserInputViewModel.kt
+++ app/src/main/java/com/example/animalchoseapplication/ui/UserInputViewModel.kt
@@ -2,11 +2,28 @@ package com.example.animalchoseapplication.ui
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.animalchoseapplication.data.UserDataUiEvents
 import com.example.animalchoseapplication.data.UserInputScreenState
 
 class UserInputViewModel:ViewModel() {
     var uiState= mutableStateOf(UserInputScreenState())
+    fun onEvent(event: UserDataUiEvents){
+        when(event){
+            is UserDataUiEvents.UserNameEntered->{
+                uiState.value= uiState.value.copy(
+                    nameEntered = event.name
+                )
+            }
+            //37.32 youtube
+            is UserDataUiEvents.AnimalSelected->{
+                uiState.value=uiState.value.copy(
+                    animalSelected = event.animalValue
+                )
+            }
+        }
+    }
 }
+
 
 
 
